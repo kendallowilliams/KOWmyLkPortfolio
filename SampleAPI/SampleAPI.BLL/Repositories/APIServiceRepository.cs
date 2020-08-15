0d17681fa60224f6c856f16042a5d719720adf20
Kendall Williams <kendallowilliams@outlook.com>
2020-08-14 22:35:33 -0400

    continuing implementation

---
+++ SampleAPI/SampleAPI.BLL/Repositories/APIServiceRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleAPI.BLL.Repositories
+{
+    public class APIServiceRepository
+    {
+        public static IEnumerable<Type> GetAPIServiceDataTypes()
+        {
+            yield return typeof(bool);
+            yield return typeof(int);
+            yield return typeof(string);
+        }
+    }
+}
