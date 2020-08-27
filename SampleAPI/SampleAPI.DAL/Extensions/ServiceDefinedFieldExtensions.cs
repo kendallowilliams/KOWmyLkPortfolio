5215ee14a35ceaadd7985b4af46a6bda134a4aff
Kendall Williams <kendallowilliams@outlook.com>
2020-08-26 21:26:35 -0400

    minor refactor

---
+++ SampleAPI/SampleAPI.DAL/Extensions/ServiceDefinedFieldExtensions.cs
@@ -0,0 +1,18 @@
+﻿using SampleAPI.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleAPI.DAL.Extensions
+{
+    public static class ServiceDefinedFieldExtensions
+    {
+        public static ServiceDefinedField GetField(this IEnumerable<ServiceDefinedField> fields, string name)
+        {
+            if (fields == null) /*then*/ fields = Enumerable.Empty<ServiceDefinedField>();
+
+            return fields.FirstOrDefault(field => field.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
