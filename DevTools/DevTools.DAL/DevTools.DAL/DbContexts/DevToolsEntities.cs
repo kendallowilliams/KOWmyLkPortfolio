70afd3feef9e3c4861a3813d93a0a5e8b2f2d20d
Kendall Williams <kendallowilliams@outlook.com>
2021-02-13 20:39:20 -0500

    added dbcontext

---
+++ DevTools/DevTools.DAL/DevTools.DAL/DbContexts/DevToolsEntities.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using DevTools.DAL.Models;
+
+#nullable disable
+
+namespace DevTools.DAL.DbContexts
+{
+    public partial class DevToolsEntities : DbContext
+    {
+        public DevToolsEntities()
+        {
+        }
+
+        public DevToolsEntities(DbContextOptions<DevToolsEntities> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<DevToolsObject> DevToolsObjects { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=kserver;Initial Catalog=DevTools;Persist Security Info=True;User ID=kserver_sql;Password=kserver_sql;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<DevToolsObject>(entity =>
+            {
+                entity.ToTable("DevToolsObject");
+
+                entity.Property(e => e.CreatedBy)
+                    .IsRequired()
+                    .HasMaxLength(128)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.CreatedOn).HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.ModifiedBy)
+                    .IsRequired()
+                    .HasMaxLength(128)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ModifiedOn).HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.ObjectJson)
+                    .IsRequired()
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ObjectType)
+                    .IsRequired()
+                    .HasMaxLength(128)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ DevTools/DevTools.DAL/DevTools.DAL/Models/DevToolsObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DevTools.DAL.Models
+{
+    public partial class DevToolsObject
+    {
+        public int Id { get; set; }
+        public Guid ObjectId { get; set; }
+        public string ObjectType { get; set; }
+        public string ObjectJson { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModifiedOn { get; set; }
+    }
+}
